bc02a318189c12c8a9e98f8ec81d48d0e960a5ee
huykemvip1 <huykemvip@gmail.com>
2022-04-18 02:13:15 +0700

    18/04 complete giao dien dang nhap,dang xuat,dang ky

--- angular_account/src/app/app-routing.module.ts
+++ angular_account/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { RegistrySuccessComponent } from './component/registry-success/registry-success.component';
+import { RegistryComponent } from './component/registry/registry.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login",component: LoginComponent},
+  {path:"registry",component: RegistryComponent},
+  {path:"registry/success",component: RegistrySuccessComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular_account/src/app/app.module.ts
+++ angular_account/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
+import { RegistryComponent } from './component/registry/registry.component';
+import { RegistrySuccessComponent } from './component/registry-success/registry-success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegistryComponent,
+    RegistrySuccessComponent
   ],
   imports: [
     BrowserModule,
