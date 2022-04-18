6e3bfde024b081126a432e0e95c273c4671b0f23
huykemvip1 <huykemvip@gmail.com>
2022-04-18 16:29:46 +0700

    Thêm giao diện ql nhân viên,khách hàng,cá nhân

--- angular_account/src/app/app-routing.module.ts
+++ angular_account/src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
+import { ManageEmployeeComponent } from './component/manage-employee/manage-employee.component';
+import { ManageUserComponent } from './component/manage-user/manage-user.component';
+import { ProfileComponent } from './component/profile/profile.component';
 import { RegistrySuccessComponent } from './component/registry-success/registry-success.component';
 import { RegistryComponent } from './component/registry/registry.component';
 
 const routes: Routes = [
   {path:"login",component: LoginComponent},
   {path:"registry",component: RegistryComponent},
-  {path:"registry/success",component: RegistrySuccessComponent}
+  {path:"registry/success",component: RegistrySuccessComponent},
+  {path:"manage_employee",component: ManageEmployeeComponent},
+  {path:"manage_customer",component:ManageUserComponent},
+  {path:"profile",component:ProfileComponent}
 ];
 
 @NgModule({
--- angular_account/src/app/app.module.ts
+++ angular_account/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import { RegistryComponent } from './component/registry/registry.component';
 import { RegistrySuccessComponent } from './component/registry-success/registry-success.component';
+import { ManageEmployeeComponent } from './component/manage-employee/manage-employee.component';
+import { ManageUserComponent } from './component/manage-user/manage-user.component';
+import { ProfileComponent } from './component/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistryComponent,
-    RegistrySuccessComponent
+    RegistrySuccessComponent,
+    ManageEmployeeComponent,
+    ManageUserComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
